6ec710c500e31eff16deb8fa7f4afdc758ecffe7
Bernard Paques <bernard.paques@gmail.com>
2017-04-08 21:59:09 +0200

    add a default command to catch unmatched verbs

---
+++ shellbot/commands/default.py
@@ -0,0 +1,57 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from base import Command
+
+class Default(Command):
+    """
+    Handles input string that no other command can process
+    """
+
+    def execute(self, verb, arguments):
+        """
+        Handles input string that no other command can process
+        """
+        self.shell.say(
+            "Sorry, I do not know how to handle '{}'".format(verb))
+        return True
+
+    @property
+    def keyword(self):
+        """
+        Retrieves the verb or token for this command
+        """
+        return '*'
+
+    @property
+    def information_message(self):
+        """
+        Retrieves basic information for this command
+        """
+        return 'Handles unmatched input string.'
+
+    @property
+    def usage_message(self):
+        """
+        Retrieves usage information for this command
+        """
+        return None
+
+    @property
+    def is_hidden(self):
+        """
+        Ensures that this command appears in help
+        """
+        return True
