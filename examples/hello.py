66920efa1129c5f4dd8c6d9fb375857d4939667b
Bernard Paques <bernard.paques@gmail.com>
2017-07-22 16:49:44 +0200

    use banner in examples/hello

--- examples/hello.py
+++ examples/hello.py
@@ -21,6 +21,20 @@ Hello, World!
 
 In this example we create a shell with one simple command: hello
 
+Multiple questions are adressed in this example:
+
+- How to build a basic command that displays a static message? Look at the
+ command ``hello`` below, this is really done in 3 lines of code.
+
+- How to use Cisco Spark? As shown below, the platform is selected during
+  the initialisation of the engine. Here we put ``type='spark'`` and that's it.
+  Ok, in addition to this code you also have to set some variables to make it
+  work, but this is configuration done outside the code itself.
+
+- How to change the banner? Shellbot support bare text, rich content, and even
+  some upload, all together. This is done with some environment variables, as
+  shown below.
+
 To run this script you have to provide a custom configuration, or set
 environment variables instead::
 
@@ -60,6 +74,9 @@ engine = Engine(type='spark', command=Hello())
 # load configuration
 #
 os.environ['CHAT_ROOM_TITLE'] = 'Hello tutorial'
+os.environ['BOT_BANNER_TEXT'] = u"Type '@{} help' for more information"
+os.environ['BOT_BANNER_CONTENT'] = u"Hello there! Type ``@{} help`` at any time to get more information on available commands."
+os.environ['BOT_BANNER_FILE'] = "https://upload.wikimedia.org/wikipedia/commons/5/5e/Sunrise_Matobo_Zimbabwe.jpg"
 engine.configure()
 
 # create a chat room
