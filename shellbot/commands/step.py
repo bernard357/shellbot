d5562ee1202736a8e4001be77a21e2c7d561f44f
Bernard Paques <bernard.paques@gmail.com>
2017-08-03 10:11:22 +0200

    expand commands/step

--- shellbot/commands/step.py
+++ shellbot/commands/step.py
@@ -23,18 +23,32 @@ class Step(Command):
     """
     Moves underlying state machine to the next step
 
-    Example::
+    This command sends an event to the current state machine. This can be
+    handled by the state machine, e.g., a ``Steps`` instance, for moving forward.
 
-        step = Step()
-        shell.load_command(step)
+    You can check ``examples/pushy.py`` as a practical tutorial.
 
-    This command only applies when the bot has a state machine.
-    You can check ``examples/escalation.py`` to get a concrete use case.
+    Example to load the command in the engine::
+
+        engine = Engine(commands=['shellbot.commands.step', ...])
+
+    By default, the command sends the event ``next`` to the state machine.
+    This can be changed while creating your own command instance, before
+    it is given to the engine. For example::
+
+        step = Steps(event='super_event')
+        engine = Engine(commands=[step, ...])
+
+    Obviously, this command should not be loaded if your use case does not rely
+    on state machines, or if your state machines do not expect events
+    from human beings.
 
     """
 
     keyword = u'step'
     information_message = u'Move process to next step'
+
+    event = 'next'  # understood by machines/steps
 
     def execute(self, bot, arguments=None, **kwargs):
         """
@@ -46,10 +60,14 @@ class Step(Command):
         :param arguments: The arguments for this command
         :type arguments: str or ``None``
 
-        This function raises AttributeError when the bot has
-        not been initialised with a suitable Machine.
+        This function calls the ``step()`` function of the underlying
+        state machine and provides a static event. It also transmits text
+        typed by the end user after the command verb, and any other
+        parameters received from the shell, e.g., attachment, etc.
         """
         if bot.machine:
-            bot.machine.step(event='next')
+            bot.machine.step(event=self.event,
+                             arguments=arguments,
+                             **kwargs)
         else:
             bot.say(u"No state machine is available")
